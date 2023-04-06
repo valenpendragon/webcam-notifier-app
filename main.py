f4d0c3e72f0bfd637f38a4468f14ceb3d9ce7464
Jeffrey Scott <valenpendragon@gmail.com>
2023-04-06 07:09:52 -0400

    Add functionality to filter images in grayscale to find objects about the size of a face entering the camera's picture.

---
+++ main.py
@@ -0,0 +1,39 @@
+import cv2
+import time
+
+video = cv2.VideoCapture(0)
+time.sleep(3)
+
+first_frame = None
+
+while True:
+    check, frame = video.read()
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
+
+    if first_frame is None:
+        first_frame = gray_frame_gau
+
+    delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
+    thresh_frame = cv2.threshold(delta_frame, 90, 255, cv2.THRESH_BINARY)[1]
+    dilate_frame = cv2.dilate(thresh_frame, None, iterations=2)
+    contours, check = cv2.findContours(dilate_frame, cv2.RETR_EXTERNAL,
+                                       cv2.CHAIN_APPROX_SIMPLE)
+
+    # We need to filter out the objects created by noise. We can do that by
+    # skipping anything that has a small area in pixels in the frame.
+    for contour in contours:
+        if cv2.contourArea(contour) < 5000:
+            continue
+        x, y, w, h = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+
+
+    cv2.imshow("My video", frame)
+
+    key = cv2.waitKey(1)
+
+    if key == ord("q"):
+        break
+
+video.release()
